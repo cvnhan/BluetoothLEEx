c66a42c664f7a04b8391dc9069b6b3338e14b639
NhanCao <caovannhan2002@gmail.com>
2015-12-14 16:31:54 +0700

    test advertising

---
+++ app/src/main/java/com/cvnhandroid/bluetoothleex/activities/AdvertieseActivity.java
@@ -0,0 +1,27 @@
+package com.cvnhandroid.bluetoothleex.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cvnhandroid.bluetoothleex.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by NhanCao on 14-Dec-15.
+ */
+public class AdvertieseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+
+
+
+    }
+}
